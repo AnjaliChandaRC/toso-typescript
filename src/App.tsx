3206b8a08047427f9f46c1761b977a27e70e606e
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-12-13 10:30:44 +0530

    add firebase store data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+// "use client";
+
 import "./App.css";
 import ToDoList from "./components/ToDoList";
 
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase/firebase";
+
 function App() {
+  const getData = async () => {
+    const querySnapshot = await getDocs(collection(db, "todo"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+  };
+  getData();
+
   return (
     <>
-      <ToDoList/>
+      <ToDoList />
     </>
   );
 }
